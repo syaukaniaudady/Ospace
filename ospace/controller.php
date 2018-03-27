db1dc0fc89522c3b732c2f080e34cfa9fde443ee
fadhilfathi <fadhilfathi.rzm@gmail.com>
2018-03-27 19:30:10 +0800

    update

--- ospace/controller.php
+++ ospace/controller.php
@@ -7,10 +7,6 @@
     function construct(){
       $this->model = new model();
     }
-    function insert($nama, $kategori, $tanggal, $isi, $namagambarFix){
-			$hasil = $this->model->query("INSERT INTO `mainwebpost` (`nama`, `kategori`, `tanggal`, `isi`, `gambar`)
-			            VALUES ('$nama', '$kategori', '$tanggal', '$isi', '$namagambarFix')");
-		}
     function index(){
 			include "index.php";
     }
